332e17379432aa4e6f86c3cc374d6938f1aa3f00
cameron-justice <cameronjustice@protonmail.com>
2020-01-25 13:05:11 -0600

    Add functionality area in views

--- forecasting/views.py
+++ forecasting/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+import spotipy
+from .models import Playlist
 
 # Create your views here.
 def forecast(request):
-    return render(request, 'forecasting/index.html', {})
+    weather = getWeather()
+    pl = getPlaylist(weather)
+    return render(request, 'forecasting/index.html', {'weather': weather, 'playlist': pl})
+
+def getWeather():
+    return ""
+
+def getPlaylist(weather):
+    return ""
