a00beb3be23dff4ec3ce4326c11ccd871d448778
cameron-justice <cameronjustice@protonmail.com>
2020-01-25 10:40:16 -0600

    Add Playlist model

--- forecasting/models.py
+++ forecasting/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Playlist(models.Model):
+    pID = models.TextField()
+    title = models.CharField(50)
+    cover = models.ImageField()
