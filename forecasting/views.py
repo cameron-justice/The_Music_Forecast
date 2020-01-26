83c2ce148b3783e59e3aee0f0146f40673424765
cameron-justice <cameronjustice@protonmail.com>
2020-01-25 20:16:04 -0600

    Finished functionality for selecting playlist

--- forecasting/views.py
+++ forecasting/views.py
@@ -1,26 +1,62 @@
 from django.shortcuts import render
 import spotipy
 from .models import Playlist
+from .models import WeatherValue
 import requests
 from spotipy.oauth2 import SpotifyClientCredentials
 import os
+import numpy as np
 
 #client_credentials_manager = SpotifyClientCredentials(client_id=os.environ.get('CLIENT_KEY'), client_secret=os.environ.get('CLIENT_SECRET'))
 
 # Create your views here.
 def forecast(request):
- #   weather = getWeather()
-  #  pl = getPlaylist(weather)
+    weather = getWeather()
+    pl = getPlaylist(weather)
     return render(request, 'forecasting/index.html', {'weather': weather, 'playlist': pl})
 
-def updateValues():
-    return 0.0
+# Functionality
+
+def updateValues(request):
+    PList = Playlist.objects.all()
 
 def getWeather():
     return ""
 
-def getPlaylist(weather):
-    return ""
+def getPlaylist(weather_vals):
+    PList = Playlist.objects.all()
+    pValues = []
+    pIDs = []
+
+    # Append the values and IDs in the same order
+    for p in PList:
+        pValues.append(calcPlaylistValues(PList.pID)
+        pIDs.append(PList.pID)
+    plIndex = getClosestPlaylist(pValues, weather_vals)
+
+    return PList.get(pID=pIDs[plIndex])
+
+def getClosestPlaylist(p_values, w_value):
+    closest = None
+    nearDist = 100000
+    index = 0
+    for line in p_values:
+        dist = (
+            abs(w_value[0] - line[0])
+            abs(w_value[1] - line[1])
+            abs(w_value[2] - line[2])
+        )
+
+        for i in range(len(dist)):
+            if dist[i] == 0:
+                dist[i] += np.random.random(.001, .005)
+
+        sDist = dist[0]*dist[1]*dist[2]
+        if sDist < nearDist:
+            nearDist = sDist
+            closest = index
+
+        index += 1
 
 def calcPlaylistValues(pID):
     playlist = sp.playlist(pID, fields = None, market = None)
