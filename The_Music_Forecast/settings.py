3a33ed48ccdc25b4fa4078b3526b0b2b18ecd0dc
cameron-justice <cameronjustice@protonmail.com>
2020-01-26 03:53:13 -0600

    statics

--- The_Music_Forecast/settings.py
+++ The_Music_Forecast/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'ro5yt-frp+!k^zx6jst(d+ww4l&h6(0-0=s3f)7-1619wm4aip'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
