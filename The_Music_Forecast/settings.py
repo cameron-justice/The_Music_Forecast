513f1fa61384136a8772bb0ff2c6c83c109d1a7f
cameron-justice <cameronjustice@protonmail.com>
2020-01-26 03:14:17 -0600

    ready for prod

--- The_Music_Forecast/settings.py
+++ The_Music_Forecast/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY='ro5yt-frp+!k^zx6jst(d+ww4l&h6(0-0=s3f)7-1619wm4aip'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ancient-sea-34849.herokuapp.com']
 
